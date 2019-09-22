6c00893a16c1bc996ee9036d076ba39c6337a8b0
Carlos Nunez <carlosonunez@noreply.github.com>
2019-09-22 17:31:58 -0500

    Add test structure

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+@sample_albums = [
+  { album_name: "We Want More", artist: "Neverlearn" },
+  { album_name: "Demo '19", artist: "Hey Maxine" },
+  { album_name: "2nd Album Sampler", artist: "Short Straw Fate" },
+  { album_name: "Late Bloomers", artist: "For I Am" },
+  { album_name: "Kept Alive", artist: "Kept Alive" },
+  { album_name: "River Monster Records Presents: MONSTER COMPSTER Vol-2", artist: "Various Artist" },
+  { album_name: "Reptile Boy", artist: "The Sunnydales" },
+  { album_name: "The Pack", artist: "The Sunnydales" },
+  { album_name: "Demo 2019", artist: "Breaking Up" },
+  { album_name: "Arrows", artist: "Free Sergio" },
+  { album_name: "New Frontiers", artist: "Burn The Boats (Boston)" },
+  { album_name: "How Did It Ever Come to This", artist: "Danvers" },
+  { album_name: "Breathe In, Bleed Out", artist: "Qualifier" },
+  { album_name: "Late Bloomers", artist: "FOR I AM" },
+  { album_name: "What did you expect? I got it off the internet!", artist: "Breakup Haircut" },
+  { album_name: "Truth Fades", artist: "YOUNG HARTS" },
+  { album_name: "Love And Lacerations", artist: "The Hostiles" },
+  { album_name: "...graffiti my coffin", artist: "latchkey kids" },
+  { album_name: "s.t.", artist: "Deconstruction Kit" }
+]
